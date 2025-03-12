454db18f64383a3b5dab8af8b2499c7f5cfcba17
Leonyxtested <benjaminMarGon@gmail.com>
2025-03-12 10:19:22 +0100

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -44,7 +44,7 @@ async function obtenerLineaAleatoriaDeArchivo(url) {
 // Función para obtener una línea aleatoria de cualquiera de los archivos en una carpeta
 async function obtenerLineaAleatoria() {
     const url = 'docs/languaje/';
-    const archivos = ['prueba.txt', 'prueba2.txt'].map(nombre => carpeta + nombre); // Lista predefinida de archivos
+    const archivos = ['prueba.txt', 'prueba2.txt']; // Lista predefinida de archivos
 
     if (archivos.length === 0) {
         return 'No se encontraron archivos en la carpeta';
